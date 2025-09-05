50804529a4530960ae1d79c800c50619c454881c
Luis Loreto <loretoleg@gmail.com>
2025-09-05 18:30:19 -0500

    Changes to be committed: 	modified:   .github/workflows/deploy.yml 	modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,9 +13,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  env: {
-    NEXT_PUBLIC_LAMBDA_URL: process.env.NEXT_PUBLIC_LAMBDA_URL,
-  },
+
   // For a root deployment like https://loretoleg.github.io/,
   // assetPrefix and basePath should be empty or not set.
   // assetPrefix: '',
